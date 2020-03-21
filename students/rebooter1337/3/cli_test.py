411b1f1ca926fa7f1dfafac92bc60877727202ff
Volkov Atyom <36457491+rebooter1337@users.noreply.github.com>
2020-03-21 15:24:11 +0300

    Update cli_test.py

--- students/rebooter1337/3/cli_test.py
+++ students/rebooter1337/3/cli_test.py
@@ -1,32 +1,32 @@
 # -*- coding: utf-8 -*-
 
-from cli import contains, ls 
-from cli import mk, rm, since
 import subprocess
+from cli import contains, ls  # noqa I001
+from cli import mk, rm, since  # noqa I001
 
 
 def test_ls(ls_fixture):
-    """ls tests."""
+    """Ls tests."""
     assert ls(ls_fixture[0]) == ls_fixture[1]
 
 
 def test_mk(mk_fixture):
-    """mk tests."""
+    """Mk tests."""
     assert mk(mk_fixture[0]) == mk_fixture[1]
 
 
 def test_rm(rm_fixture):
-    """rm tests."""
+    """Rm tests."""
     assert rm(rm_fixture[0]) == rm_fixture[1]
 
 
 def test_contains(contains_fixture):
-    """contains tests."""
+    """Contains tests."""
     assert contains(contains_fixture[0]) == contains_fixture[1]
 
 
 def test_since(since_fixture):
-    """since tests."""
+    """Since tests."""
     assert since(
         since_fixture[0],
         since_fixture[1],
@@ -36,6 +36,6 @@ def test_since(since_fixture):
 def test_integration(integration_fixture):
     """Integration tests."""
     command, parameter = integration_fixture
-    format_str  = 'python students/rebooter1337/3/cli.py {0} {1}'
-    command_str  = format_str .format(command, parameter)
+    format_str = 'python students/rebooter1337/3/cli.py {0} {1}'
+    command_str = format_str .format(command, parameter)
     assert subprocess.call(command_str , shell=True) == 0
