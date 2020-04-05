ffb22aa3fcc8901b682f19c4a61bd05c246c056a
Volkov Atyom <36457491+rebooter1337@users.noreply.github.com>
2020-04-05 10:33:25 +0300

    Update test_cli.py

--- students/revyakinpetr/3/test_cli.py
+++ students/revyakinpetr/3/test_cli.py
@@ -35,7 +35,7 @@ def test_rm(rm_fixture):
 def test_contains(contains_fixture):
     """Testing contains command."""
     filename, status = contains_fixture
-    assert contains(filename) == ['code: 1']
+    assert contains(filename) == ['code: 0']
 
 
 def test_since(since_fixture):
