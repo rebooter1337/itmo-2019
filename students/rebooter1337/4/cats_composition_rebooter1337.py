30f9e5292236a24a54c5c787beefa336e7a7793f
Volkov Atyom <36457491+rebooter1337@users.noreply.github.com>
2020-04-05 11:57:38 +0300

    Add files via upload

---
+++ students/rebooter1337/4/cats_composition_rebooter1337.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+# type: ignore  # noqa E800
+
+from typing import Callable, Tuple
+
+from requests.packages import urllib3
+
+from cats_direct_rebooter1337 import (
+    create_parser,
+    fetch_cat_fact,
+    fetch_cat_image,
+    save_cat,
+)
+
+
+class CatProcessor(object):
+    """
+    Knows exactly how to process cats.
+    Only uses composition.
+    """
+
+    def __init__(
+        self,
+        fetch_text,
+        fetch_image: Callable[[], Tuple[str, urllib3.HTTPResponse]],
+        process_text_and_image: Callable[[int, str, Tuple[str, urllib3.HTTPResponse]], None]  # noqa E501
+    ):
+        """Saves dependencies into internal state."""
+        self._fetch_text = fetch_text
+        self._fetch_image = fetch_image
+        self._process_text_and_image = process_text_and_image
+
+    def __call__(self, index: int):
+        """Runs the process of cat downloading."""
+        return self._process_text_and_image(
+            index,
+            self._fetch_text(),
+            self._fetch_image(),
+        )
+
+
+def main(
+    cats_to_fetch: int,
+    process_cat: Callable,
+    show_information: Callable,
+) -> None:
+    """Fetches cats and saves the into temp folder."""
+    if not cats_to_fetch:
+        show_information('No cats :(')
+        return
+
+    for cat_index in range(1, cats_to_fetch + 1):
+        process_cat(cat_index)
+    show_information('Cats downloaded!')
+
+
+if __name__ == '__main__':
+    # Building dependencies:
+    cat_processor = CatProcessor(fetch_cat_fact, fetch_cat_image, save_cat)
+
+    # Building our main:
+    main(
+        create_parser().parse_args().count,
+        process_cat=cat_processor,
+        show_information=print,  # noqa: T002
+    )
---
+++ students/rebooter1337/4/test_cats_composition_rebooter1337.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+
+import os
+import shutil
+import subprocess
+import unittest
+
+import pytest
+
+from cats_composition_rebooter1337 import CatProcessor, main
+from cats_direct_rebooter1337 import fetch_cat_fact, fetch_cat_image, save_cat
+
+
+class TestCatsComposition(unittest.TestCase):
+    """Composition unittest."""
+
+    def setUp(self):
+        """SetUp."""
+        self.index = 5
+        self.temp = 'temp'
+        if not os.path.exists(self.temp):
+            os.mkdir(self.temp)
+        self.str_format = 'python cats_composition_rebooter1337.py {0}'
+        self.integration_arg = '--count=2'
+
+    def tearDown(self):
+        """Tear down test case."""
+        shutil.rmtree(self.temp)
+
+    @pytest.mark.remote_data
+    def test_main(self):
+        """Test main function."""
+        cat_processor = CatProcessor(
+            fetch_text=fetch_cat_fact,
+            fetch_image=fetch_cat_image,
+            process_text_and_image=save_cat,
+        )
+
+        fact_path = '{0}/cat_{1}_fact.txt'.format(self.temp, self.index)
+        if os.path.exists(fact_path):
+            os.remove(fact_path) 
+
+        main(
+            self.index,
+            process_cat=cat_processor,
+            show_information=print,
+        )
+
+        assert os.path.exists(fact_path)
+
+    def test_integration(self):
+        """Integration test."""
+        str_command = self.str_format.format(self.integration_arg)
+        subprocess_res = subprocess.call(str_command, shell=True)
+        assert subprocess_res == 0
+
+
+if __name__ == '__main__':
+    unittest.main()
