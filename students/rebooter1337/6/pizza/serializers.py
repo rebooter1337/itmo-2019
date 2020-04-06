97c054d6f8eb183a05d266a85d952f2aa0814906
Volkov Atyom <36457491+rebooter1337@users.noreply.github.com>
2020-04-06 14:20:58 +0300

    Update serializers.py

--- students/rebooter1337/6/pizza/serializers.py
+++ students/rebooter1337/6/pizza/serializers.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from rest_framework.serializers import ModelSerializer
-
 from pizza.models import Ingredient, Order, Pizza
+from rest_framework.serializers import ModelSerializer
 
 
 class IngredientSerializer(ModelSerializer):
