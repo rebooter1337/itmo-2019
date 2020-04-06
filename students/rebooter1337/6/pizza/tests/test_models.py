145fb3b2a697c63f5f1b2e2a6c4168f22431b0ce
Volkov Atyom <36457491+rebooter1337@users.noreply.github.com>
2020-04-06 16:16:02 +0300

    Update test_models.py

--- students/rebooter1337/6/pizza/tests/test_models.py
+++ students/rebooter1337/6/pizza/tests/test_models.py
@@ -2,7 +2,7 @@
 
 from django.test import TestCase
 
-from ..models import Ingredient, Pizza  # noqa: WPS300
+from pizza.models import Ingredient, Pizza  # noqa: WPS300
 
 
 class IngredientTest(TestCase):
