ccfb133e070f55c327783d214438f233c471b91e
Volkov Atyom <36457491+rebooter1337@users.noreply.github.com>
2020-04-06 20:28:40 +0300

    Update wsgi.py

--- students/rebooter1337/6/pizzapp/wsgi.py
+++ students/rebooter1337/6/pizzapp/wsgi.py
@@ -2,7 +2,9 @@
 
 """
 WSGI config for pizza project.
+
 It exposes the WSGI callable as a module-level variable named ``application``.
+
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
