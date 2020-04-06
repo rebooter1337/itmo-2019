5e3a76098acd57531e44ea4d98abc1e03695b052
Volkov Atyom <36457491+rebooter1337@users.noreply.github.com>
2020-04-06 14:15:19 +0300

    Update test_views.py

--- students/rebooter1337/6/pizza/tests/test_views.py
+++ students/rebooter1337/6/pizza/tests/test_views.py
@@ -1,4 +1,7 @@
+# -*- coding: utf-8 -*-
+
 from django.test import TestCase
+
 from ..models import Ingredient, Pizza
 
 
@@ -8,47 +11,54 @@ class PostTest(TestCase):
     def setUp(self):
         """Setting up database for testing purpose."""
         Ingredient.objects.create(
-            title='Dough')
+            title='Dough'
+        )
         Ingredient.objects.create(
-            title='Garlic')
+            title='Garlic'
+        )
         royal = Pizza.objects.create(
-            title='Royal common', price=200)
+            title='Royal common', price=200  # noqa: WPS432
+        )
         royal.ingredients.set([1])
 
-        margaret = Pizza.objects.create(title='Margaret', price=90)
+        margaret = Pizza.objects.create(title='Margaret', price=90)  # noqa: WPS432
         margaret.ingredients.set([2])
 
     def test_stats(self):
         """Checks accessibility of order API."""
-        data = {
-            "pizzas": [0],
-            "delivery_address": "murinskaya 61",
-            "customer_email": "artvolk981@gmail.com"
+        datas = {
+            'pizzas': [0],
+            'delivery_address': 'murinskaya 61',
+            'customer_email': 'artvolk981@gmail.com',
         }
 
-        response = self.client.post(path='/api/order/', data=data)
-        assert response.status_code == 200
+        response = self.client.post(path='/api/order/', datas1=datas)
+        assert response.status_code == 200  # noqa: WPS432
 
+        
 class TestGetPizzas(TestCase):
     """Testing Views getting API Pizza class."""
 
     def setUp(self):
         """Setting up database for testing purpose."""
         Ingredient.objects.create(
-            title='Dough')
+            title='Dough'
+        )
         Ingredient.objects.create(
-            title='Garlic')
+            title='Garlic'
+        )
         royal = Pizza.objects.create(
-            title='Royal common', price=200)
+            title='Royal common', price=200  # noqa: WPS432
+        )
         royal.ingredients.set([1])
 
-        margaret = Pizza.objects.create(title='Margaret', price=90)
+        margaret = Pizza.objects.create(title='Margaret', price=90  #noqa: WPS432
         margaret.ingredients.set([2])
 
     def test_get(self):
         """Testing Views getting API Pizza."""
         response = self.client.get(path='/api/pizza/')
-        assert response.status_code == 200
+        assert response.status_code == 200  #noqa: WPS432
 
 
 class TestStatistics(TestCase):
@@ -57,30 +67,32 @@ class TestStatistics(TestCase):
     def setUp(self):
         """Setting up database for testing purpose."""
         Ingredient.objects.create(
-            title='Dough')
+            title='Dough'
+        )
         Ingredient.objects.create(
-            title='Garlic')
+            title='Garlic'
+        )
         royal = Pizza.objects.create(
-            title='Royal common', price=200)
+            title='Royal common', price=200  #noqa: WPS432
+        )
         royal.ingredients.set([1])
 
-        margaret = Pizza.objects.create(title='Margaret', price=90)
+        margaret = Pizza.objects.create(title='Margaret', price=90)  #noqa: WPS432
         margaret.ingredients.set([2])
-        data = {
-            "pizzas": [1],
-            "delivery_address": "murinskaya 61",
-            "customer_email": "artvolk981@gmail.com"
+        datas = {
+            'pizzas': [1],
+            'delivery_address': 'murinskaya 61',
+            'customer_email': 'artvolk981@gmail.com',
         }
 
-        self.client.post(path='/api/order/', data=data)
+        self.client.post(path='/api/order/', datas1=datas)
 
     def test_loading(self):
         """Tests for load."""
         response = self.client.get('/api/statistics/pizza')
-        assert response.status_code == 200
+        assert response.status_code == 200  # noqa: WPS432
         assert response.json() == {
             'all': 1,
             'pizzas': {'1': 1},
-            'statuses': {'ACCEPTED': 1, 'COOKING': 0, 'DELIVERY': 0, 'FINISHED': 0},
+            'statuses': {'ACCEPTED': 1, 'COOKING': 0, 'DELIVERY': 0, 'FINISHED': 0},  # noqa: E501
         }
-
